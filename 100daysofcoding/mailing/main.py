afab993cd904035d65d94d152d62eaedea1bcc0c
Oleksandr Balkashyn -X (obalkash - SOFTSERVE INC at CISCO) <obalkash@cisco.com>
2022-01-31 21:51:29 +0200

    Updated mailing

--- 100daysofcoding/mailing/main.py
+++ 100daysofcoding/mailing/main.py
@@ -1,6 +1,9 @@
+REPLACER = "[NAME]"
 with open("./input/names/invited_names") as names:
-    for name in names:
-        with open("./input/letters/started_letter") as letter:
-            updated_letter = letter.read().replace("[NAME]", name)
-            with open(f"./output/ready_to_send/letter_for_{name}", mode="w") as final_letter:
-                final_letter.write(updated_letter)
+    all_names = names.readlines()
+for name in all_names:
+    with open("./input/letters/started_letter") as letter:
+        stripped_name = name.strip()
+        updated_letter = letter.read().replace(REPLACER, stripped_name)
+        with open(f"./output/ready_to_send/letter_for_{stripped_name}", mode="w") as final_letter:
+            final_letter.write(updated_letter)
