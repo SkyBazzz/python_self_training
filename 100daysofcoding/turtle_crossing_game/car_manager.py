699fc830c0433cc80d568fa823a692b1d361bb78
Oleksandr Balkashyn -X (obalkash - SOFTSERVE INC at CISCO) <obalkash@cisco.com>
2022-01-31 18:32:53 +0200

    created turtle crossing game

---
+++ 100daysofcoding/turtle_crossing_game/car_manager.py
@@ -0,0 +1,32 @@
+import random
+from turtle import Turtle
+from typing import List
+
+COLORS = ["green", "red", "orange", "yellow", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+class CarManager:
+
+    def __init__(self) -> None:
+        self.all_cars: List[Turtle] = list()
+        self.car_speed = STARTING_MOVE_DISTANCE
+
+    def create_cars(self):
+        random_dice = random.randint(1, 6)
+        if random_dice == 1:
+            car = Turtle("square")
+            car.shapesize(stretch_wid=1, stretch_len=2)
+            car.penup()
+            car.color(random.choice(COLORS))
+            random_y = random.randint(-250, 250)
+            car.goto(300, random_y)
+            self.all_cars.append(car)
+
+    def move(self):
+        for car in self.all_cars:
+            car.backward(self.car_speed)
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
---
+++ 100daysofcoding/turtle_crossing_game/player.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+Y_FINISH_LINE = 280
+
+
+class Player(Turtle):
+
+    def __init__(self) -> None:
+        super().__init__(shape="turtle")
+        self.color("green")
+        self.penup()
+        self.left(90)
+        self.go_to_start()
+
+    def move(self):
+        self.forward(MOVE_DISTANCE)
+
+    def is_at_finish_line(self):
+        return self.ycor() > Y_FINISH_LINE
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
