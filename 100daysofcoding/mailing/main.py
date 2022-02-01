2ad43a3ce77aba05980fd8190994e1aa9973bded
Oleksandr Balkashyn -X (obalkash - SOFTSERVE INC at CISCO) <obalkash@cisco.com>
2022-02-01 12:54:16 +0200

    renamed variable

--- 100daysofcoding/mailing/main.py
+++ 100daysofcoding/mailing/main.py
@@ -1,9 +1,9 @@
-REPLACER = "[NAME]"
+PLACEHOLDER = "[NAME]"
 with open("./input/names/invited_names") as names:
     all_names = names.readlines()
 for name in all_names:
     with open("./input/letters/started_letter") as letter:
         stripped_name = name.strip()
-        updated_letter = letter.read().replace(REPLACER, stripped_name)
+        updated_letter = letter.read().replace(PLACEHOLDER, stripped_name)
         with open(f"./output/ready_to_send/letter_for_{stripped_name}", mode="w") as final_letter:
             final_letter.write(updated_letter)
