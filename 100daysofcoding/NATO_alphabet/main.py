3c4ace55de776dcee31823fac0723399a32bd96f
Oleksandr Balkashyn -X (obalkash - SOFTSERVE INC at CISCO) <obalkash@cisco.com>
2022-02-06 11:53:40 +0200

    handle no alphabet letter exception

--- 100daysofcoding/NATO_alphabet/main.py
+++ 100daysofcoding/NATO_alphabet/main.py
@@ -4,5 +4,16 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 nato_letters = {value.letter: value.code for (index, value) in data.iterrows()}
 
-user_word = input("Enter a word: ").upper()
-print([nato_letters[letter] for letter in user_word])
+
+def generate_phonetic():
+    user_word = input("Enter a word: ").upper()
+    try:
+        output_list = ([nato_letters[letter] for letter in user_word])
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
