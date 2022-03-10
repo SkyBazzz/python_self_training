ce52b6f3bc95e6103b0a586b0fa714c740a439c7
Oleksandr Balkashyn -X (obalkash - SOFTSERVE INC at CISCO) <obalkash@cisco.com>
2022-03-10 13:31:28 +0100

    # Refactored Selenium module # Added library to requirements.txt

--- selenium_train/demo.py
+++ selenium_train/demo.py
@@ -5,16 +5,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.support.ui import Select
 
 
-class every_downloads_chrome:
-    def __call__(self, driver):
-        if not driver.current_url.startswith("chrome://downloads"):
-            driver.get("chrome://downloads/")
-        return driver.execute_script(
+class EveryDownloadsChrome:
+    def __call__(self, browser):
+        if not browser.current_url.startswith("chrome://downloads"):
+            browser.get("chrome://downloads/")
+        return browser.execute_script(
             """
                var items = document.querySelector('downloads-manager')
                    .shadowRoot.getElementById('downloadsList').items;
@@ -28,13 +26,13 @@ class every_downloads_chrome:
 driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
 driver.get("https://wiki.python.org/moin/FrontPage")
 driver.maximize_window()
-search_field = driver.find_element_by_xpath("//input[@id='searchinput']")
+search_field = driver.find_element(By.XPATH, "//input[@id='searchinput']")
 search_field.send_keys("Beginner")
 search_field.send_keys(Keys.RETURN)
-more_actions = Select(driver.find_element_by_xpath("//select[@name='action']"))
+more_actions = Select(driver.find_element(By.XPATH, "//select[@name='action']"))
 
 more_actions.select_by_visible_text("Raw Text")
 # time.sleep(5)
-path = WebDriverWait(driver, 5).until(every_downloads_chrome())
+path = WebDriverWait(driver, 5).until(EveryDownloadsChrome())
 time.sleep(25)
 driver.quit()
