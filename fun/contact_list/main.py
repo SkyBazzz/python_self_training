c3dac7d86258551647be31da8318ce460130a49c
Oleksandr Balkashyn -X (obalkash - SOFTSERVE INC at CISCO) <obalkash@cisco.com>
2022-03-30 10:32:06 +0200

    # Added skeleton for contact list project # updated requirements.txt

---
+++ fun/contact_list/main.py
@@ -0,0 +1,46 @@
+import json
+
+from fun.contact_list.models import Contact, UnknownContact
+
+
+def add_contact_info():
+    contact = form_contact_to_add()
+
+    contact_to_add = {
+        contact.full_name(): json.dumps(contact)
+    }
+    update_contacts_list(contact_to_add)
+
+
+def form_contact_to_add():
+    name = input("Input name: ").strip().title()
+    surname = input("Input surname: ").strip().title()
+    phone_number = input("Input phone number: ")
+    contact = Contact(name, surname, phone_number)
+    return contact
+
+
+def update_contacts_list(data):
+    try:
+        with open("contact_list.json", mode="r", encoding="UTF-8") as contacts:
+            contacts_info: dict = json.load(contacts)
+    except FileNotFoundError:
+        with open("contact_list.json", mode="w", encoding="UTF-8") as contacts:
+            json.dump(data, contacts, indent=2)
+    else:
+        with open("contact_list.json", mode="w", encoding="UTF-8") as contacts:
+            contacts_info.update(data)
+            json.dump(contacts_info, contacts, indent=2)
+
+
+def show_contact_info(full_name: str):
+    try:
+        with open("contact_list.json", mode="r", encoding="UTF-8") as contacts:
+            contacts_info: dict = json.load(contacts)
+    except FileNotFoundError:
+        print("Contact list doesn't exist, add contact first")
+    else:
+        return f"{Contact(**contacts_info.get(full_name, UnknownContact().__dict__))!s}"
+
+
+print(show_contact_info("Oleksandr Balkashyn"))
---
+++ fun/contact_list/models.py
@@ -0,0 +1,17 @@
+class Contact:
+
+    def __init__(self, name: str, surname: str, phone_number: str) -> None:
+        self.name = name
+        self.surname = surname
+        self.phone_number = phone_number
+
+    def full_name(self):
+        return f"{self.name} {self.surname}"
+
+    def __str__(self):
+        return f"User:\n\tname: {self.name}\n\tsurname: {self.surname}\n\tphone number: {self.phone_number}"
+
+
+class UnknownContact(Contact):
+    def __init__(self) -> None:
+        super().__init__("Unknown", "Unknown", "Empty")
