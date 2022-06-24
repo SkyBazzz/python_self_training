025948d454a8b3b2df0e61bae9d07d7efe564e39
Oleksandr Balkashyn -X (obalkash - SOFTSERVE INC at CISCO) <obalkash@cisco.com>
2022-06-24 14:29:51 +0200

    # Habit tracker with pixe.la

---
+++ 100daysofcoding/habbit_tracker/main.py
@@ -0,0 +1,33 @@
+import requests
+import datetime
+BASE_URL = "https://pixe.la/v1/"
+API_TOKEN = "scnsiiI@&HNNSKANX@*(!XNLL"
+USER_NAME = "oleksandrsky"
+RUN_GRAPH_ID = "run1"
+
+users = "users"
+graphs = "graphs"
+
+# user_params = {
+#     "token": API_TOKEN,
+#     "username": "oleksandrsky",
+#     "agreeTermsOfService": "yes",
+#     "notMinor": "yes",
+#
+# }
+
+# response = requests.post(url=f"{BASE_URL}{users}", json=user_params)
+# print(response.text)
+pixel_params = {
+    "date": f"{datetime.datetime.now().strftime('%Y%m%d')}",
+    "quantity": "2.5",
+    "optionalData": "{ 'How do you feel': 'fine'}",
+}
+
+headers = {
+    "X-USER-TOKEN": API_TOKEN
+}
+
+response = requests.post(url=f"{BASE_URL}{users}/{USER_NAME}/{graphs}/{RUN_GRAPH_ID}", json=pixel_params,
+                         headers=headers)
+print(response.text)
