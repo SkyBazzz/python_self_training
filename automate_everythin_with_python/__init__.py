80990da1a344c1bd7e1241dbf74855eb167a2aa9
Oleksandr Balkashyn -X (obalkash - SOFTSERVE INC at CISCO) <obalkash@cisco.com>
2023-04-04 14:50:08 +0300

    Added application: convert input currency into selected. So I found that if you create a system file in your project directory with the file named .pylintrc the rc file can whitelist this package to stop throwing errors by adding the following code in the rc file PyQt6

--- automate_everythin_with_python/__init__.py
+++
@@ -1,4 +0,0 @@
-"""This module is a representation of Udemy course
-    https://softserve.udemy.com/course/automate-everything-with-python
-    each directory inside is a single automation project regarding to a specific topic.
-"""
